e266e85eb6f3513522d9bb3435632317169860a2
Gerardo Rodriguez <garc@outlook.com>
2024-02-12 00:34:36 -0700

    Added Navigator and RN Icons

---
+++ src/presentation/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {AirplaneIcon} from '../icons/';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <AirplaneIcon />
+    </View>
+  );
+};
